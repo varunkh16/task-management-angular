d06e3e30fa9b7a4248f5bb2b193b9387b5075bd8
Varun Kodigehalli Hanumanth Raju -X (vkodigeh - INFOSYS LIMITED at Cisco) <vkodigeh@cisco.com>
2020-03-06 19:25:35 +0530

    Second commit from office

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
